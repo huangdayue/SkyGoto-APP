5f05efd1471777b1aee817c26a8db125004eefc3
SkyGoto <app@skygoto.local>
2026-05-22 22:54:59 +0800

    fix: 添加阿里云 Maven 镜像解决 GitHub Actions 构建网络问题

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,6 +1,9 @@
 pluginManagement {
     repositories {
         google()
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
         maven { url = uri("https://repo.maven.apache.org/maven2") }
         gradlePluginPortal()
         maven { url = uri("https://chaquo.com/maven") }
@@ -10,6 +13,9 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
         maven { url = uri("https://repo.maven.apache.org/maven2") }
         maven { url = uri("https://chaquo.com/maven") }
     }
