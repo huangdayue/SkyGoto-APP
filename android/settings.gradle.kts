6fffbec4f3bafe8dd650e9f7fb9882dc85a43c81
SkyGoto <app@skygoto.local>
2026-05-22 23:29:07 +0800

    fix: 仅使用阿里云Maven镜像构建AGP插件

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,12 +1,8 @@
 pluginManagement {
     repositories {
-        google()
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/central") }
         maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { url = uri("https://repo.maven.apache.org/maven2") }
-        gradlePluginPortal()
-        maven { url = uri("https://chaquo.com/maven") }
     }
 }
 dependencyResolutionManagement {
@@ -15,7 +11,6 @@ dependencyResolutionManagement {
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/central") }
         maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { url = uri("https://repo.maven.apache.org/maven2") }
         maven { url = uri("https://chaquo.com/maven") }
     }
 }
